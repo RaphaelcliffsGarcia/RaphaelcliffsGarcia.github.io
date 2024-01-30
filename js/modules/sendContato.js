5dbb46d212571f02f30313ac9d417ede2daa2aa1
Raphael Garcia <cliffsdesigner@gmail.com>
2024-01-30 12:58:05 -0300

    Mensagem do commit

--- js/modules/sendContato.js
+++ js/modules/sendContato.js
@@ -1,71 +1,77 @@
-// Declaração da variável divMessage fora da função sendMail
-const divMessage = document.querySelector(".alert");
+function emailmod() {
+  const divMessage = document.querySelector(".alert");
 
-function sendMail(event) {
-  event.preventDefault();
+  function sendMail(event) {
+    event.preventDefault();
 
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const message = document.getElementById("message").value;
-  const assunto = document.getElementById("assunto").value;
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+    const assunto = document.getElementById("assunto").value;
 
-  if (!name || !email || !message || !assunto) {
-    alert("Por favor, preencha todos os campos obrigatórios.");
-    return;
+    if (!name || !email || !message || !assunto) {
+      alert("Por favor, preencha todos os campos obrigatórios.");
+      return;
+    }
+
+    if (!isValidEmail(email)) {
+      alert("Por favor, insira um endereço de e-mail válido.");
+      return;
+    }
+
+    emailjs.init("lGXV7mbqVLBXoDusm");
+
+    const params = {
+      name: name,
+      email: email,
+      message: message,
+      assunto: assunto,
+    };
+
+    const serviceID = "service_2n3qxjr";
+    const templateID = "template_ii63jje";
+
+    emailjs
+      .send(serviceID, templateID, params)
+      .then((data) => {
+        const mensagem = "O seu email foi enviado"; // Substitua a mensagem aqui
+        ativar(mensagem, "success");
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error sending email:", error);
+      });
   }
 
-  if (!isValidEmail(email)) {
-    alert("Por favor, insira um endereço de e-mail válido.");
-    return;
+  function ativar(msg, type) {
+    const message = document.createElement("div");
+    message.classList.add("message", type);
+
+    // Adicione o ícone de check verde usando Font Awesome
+    const icon = document.createElement("i");
+    icon.classList.add("fa", "fa-check");
+    message.appendChild(icon);
+
+    // Adicione a mensagem de sucesso
+    const text = document.createElement("span");
+    text.innerText = msg;
+    message.appendChild(text);
+
+    divMessage.appendChild(message);
+
+    setTimeout(() => {
+      message.style.display = "none";
+    }, 3000);
   }
 
-  emailjs.init("lGXV7mbqVLBXoDusm");
-
-  const params = {
-    name: name,
-    email: email,
-    message: message,
-    assunto: assunto,
-  };
-
-  const serviceID = "service_2n3qxjr";
-  const templateID = "template_ii63jje";
-
-  emailjs
-    .send(serviceID, templateID, params)
-    .then((data) => {
-      const mensagem = "O seu email foi enviado"; // Substitua a mensagem aqui
-      ativar(mensagem, "success");
-      console.log(data);
-    })
-    .catch((error) => {
-      console.error("Error sending email:", error);
-    });
+  function isValidEmail(email) {
+    return email.includes("@");
+  }
+  const submitButton = document.querySelector(".contatoBtn");
+  submitButton.addEventListener("click", function (event) {
+    // Chame a função sendMail passando o objeto de evento
+    sendMail(event);
+  });
 }
 
-function ativar(msg, type) {
-  const message = document.createElement("div");
-  message.classList.add("message", type);
-
-  // Adicione o ícone de check verde usando Font Awesome
-  const icon = document.createElement("i");
-  icon.classList.add("fa", "fa-check");
-  message.appendChild(icon);
-
-  // Adicione a mensagem de sucesso
-  const text = document.createElement("span");
-  text.innerText = msg;
-  message.appendChild(text);
-
-  divMessage.appendChild(message);
-
-  setTimeout(() => {
-    message.style.display = "none";
-  }, 3000);
-}
-
-function isValidEmail(email) {
-  return email.includes("@");
-}
-
-export default sendMail;
+export default emailmod;
--- js/script.js
+++ js/script.js
@@ -1,16 +1,12 @@
 // Importando a função setupPage do arquivo onde você a definiu
 import setupPage from "./modules/theme-switcher.js";
 import conhecimento from "./modules/conhecimento.js";
-import sendMail from "./modules/sendContato.js";
+import emailmod from "./modules/sendContato.js";
 import suaveScroll from "./modules/scrollsuave.js";
 
 // Chame a função setupPage para configurar a página
 
 setupPage();
 conhecimento();
-const submitButton = document.querySelector(".contatoBtn");
-submitButton.addEventListener("click", function (event) {
-  // Chame a função sendMail passando o objeto de evento
-  sendMail(event);
-});
+emailmod();
 suaveScroll();
